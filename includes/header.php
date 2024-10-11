fab022c541f752f78580b8fcc2d27c2b39378355
Flavio Ricardo <fhricardo@gmail.com>
2024-10-11 14:22:00 -0300

    foreach em PHP: Update 01

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usando Foreach</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <div class="logo col-3">
+            <h1>
+                Foreach em PHP
+            </h1>
+        </div>
+        <menu class="col-9">
+            <p>Professor Flávio Ricardo</p>
+        </menu>
+    </header>
+    <main>
